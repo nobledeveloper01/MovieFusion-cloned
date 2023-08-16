379c52ec4bd5725393f4ef9a875f54a860af7585
nobledeveloper01 <nobleconcepts22@gmail.com>
2023-08-16 23:20:58 +0100

    commited changes

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'd9d481df71c0468577bde68179d2a0bc',
+    apiKey: 'Your API KEY',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
